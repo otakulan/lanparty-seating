c1818e24a1e7a05fc40ef042d4883053c9714840
Tristan Gosselin-Hane <starcraft66@gmail.com>
2018-08-12 17:34:41 -0400

    Added basic bootstrap scaffolding

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -2,25 +2,16 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
+      joinTo: {
+        'js/vendor.js': /^(?!js)/, // Files that are not in `js` dir.
+        'js/app.js': /^js/
+      }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+        joinTo: 'css/app.css',
+        order: {
+          after: ["css/app.scss"]
+        }
     },
     templates: {
       joinTo: "js/app.js"
@@ -47,7 +38,13 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
-    }
+    },
+    sass: {
+      options: {
+        includePaths: ["node_modules/bootstrap/scss"],
+        precision: 8
+      }
+    }    
   },
 
   modules: {
@@ -57,6 +54,10 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: {
+      $: 'jquery',
+      jQuery: 'jquery'
+    }
   }
 };
