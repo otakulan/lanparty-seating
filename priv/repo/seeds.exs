21d6385782c52b0b02497d367dab9e0113e793c3
Tristan Gosselin-Hane <starcraft66@gmail.com>
2018-08-14 20:00:12 -0400

    Added some basic database seeds for development

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,49 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# A few regular reservations
+
+Lanpartyseating.Repo.insert!(%Lanpartyseating.Reservation{
+  UID: "E0040150BD357501",
+  row: 2,
+  column: 1
+})
+
+Lanpartyseating.Repo.insert!(%Lanpartyseating.Reservation{
+  UID: "E0040150BD357501",
+  row: 3,
+  column: 4
+})
+
+Lanpartyseating.Repo.insert!(%Lanpartyseating.Reservation{
+  UID: "E0040150BD357501",
+  row: 1,
+  column: 3
+})
+
+Lanpartyseating.Repo.insert!(%Lanpartyseating.Reservation{
+  UID: "E0040150BD357501",
+  row: 2,
+  column: 6
+})
+
+Lanpartyseating.Repo.insert!(%Lanpartyseating.Reservation{
+  UID: "E0040150BD357501",
+  row: 9,
+  column: 1
+})
+
+# Far away reservations
+
+Lanpartyseating.Repo.insert!(%Lanpartyseating.Reservation{
+  UID: "E0040150BD357501",
+  row: 99,
+  column: 99
+})
+
+Lanpartyseating.Repo.insert!(%Lanpartyseating.Reservation{
+  UID: "E0040150BD357501",
+  row: 62,
+  column: 130
+})
