f288b730737992adf03cd7fbe4b0ba8992859e01
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2022-07-13 23:08:15 -0400

    Working cell padding functionality

--- lib/lanpartyseating_web/live/settings/settings.ex
+++ lib/lanpartyseating_web/live/settings/settings.ex
@@ -6,8 +6,10 @@ defmodule LanpartyseatingWeb.SettingsControllerLive do
     socket = socket
     |> assign(:columns, 12)
     |> assign(:rows, 12)
-    |> assign(:col_trailing, true)
-    |> assign(:row_trailing, false)
+    |> assign(:col_trailing, 0)
+    |> assign(:row_trailing, 0)
+    |> assign(:colpad, 1)
+    |> assign(:rowpad, 1)
     {:ok, socket}
   end
 
@@ -21,20 +23,27 @@ defmodule LanpartyseatingWeb.SettingsControllerLive do
 
   def handle_event("col_trailing", _params, socket) do
     socket = socket
-    |> update(:col_trailing, &(!&1))
+    |> update(:col_trailing, &(&1+1)) # fixme: integer overflow warning
     {:noreply, socket}
   end
 
   def handle_event("row_trailing", _params, socket) do
     socket = socket
-    |> update(:row_trailing, &(!&1))
+    |> update(:row_trailing, &(&1+1)) # fixme: integer overflow warning
     {:noreply, socket}
   end
 
   def handle_event("change_dimensions", %{"rows" => rows, "columns" => columns}, socket) do
     socket = socket
     |> assign(:rows, String.to_integer(rows))
-    |> assign(:columns,String.to_integer(columns))
+    |> assign(:columns, String.to_integer(columns))
+    {:noreply, socket}
+  end
+
+  def handle_event("change_padding", %{"rowpad" => rowpad, "colpad" => colpad}, socket) do
+    socket = socket
+    |> assign(:rowpad, String.to_integer(rowpad)) # fixme: colpad should not be bigger than "rows"
+    |> assign(:colpad, String.to_integer(colpad)) # fixme: colpad should not be bigger than "columns"
     {:noreply, socket}
   end
 
