fc31350e2aac04a3d91e4485a4535864d5e61b4c
Remi Martel <remi.martel.37@gmail.com>
2022-07-31 17:45:14 -0400

    misc

--- lib/lanpartyseating/logic/station_logic.ex
+++ lib/lanpartyseating/logic/station_logic.ex
@@ -17,15 +17,19 @@ defmodule Lanpartyseating.StationLogic do
 
     station = Station
     |> where(id: ^stationId)
+    # |> where(deleted_at: nil)
     |> Repo.one()
 
     latestReservation = Reservation
     |> where(station_id: ^stationId)
     |> Repo.one()
 
-    # case Repo.update settings do
-    #   {:ok, struct}       -> settings
-    #   {:error, changeset} -> nil
-    # end
+    end_time = DateTime.add(latestReservation.start_time, :minute, latestReservation.duration)
+
+    cond do
+      latestReservation.start_time <= now && end_time > now -> "occupied"
+      station.is_closed -> "closed"
+      true -> "unknown"
+    end
   end
 end
