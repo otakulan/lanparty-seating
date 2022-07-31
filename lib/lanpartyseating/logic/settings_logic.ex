e2e4f10ce4d3e910b4cc1bb7f7544238d1c2a932
Remi Martel <remi.martel.37@gmail.com>
2022-07-31 16:51:35 -0400

    added save_settings

--- lib/lanpartyseating/logic/settings_logic.ex
+++ lib/lanpartyseating/logic/settings_logic.ex
@@ -6,4 +6,18 @@ defmodule Lanpartyseating.SettingsLogic do
     |> last(:inserted_at)
     |> Lanpartyseating.Repo.one()
   end
+
+  def save_settings(rows, columns, row_padding, column_padding, horizontal_trailing, vertical_trailing) do
+    settings = Lanpartyseating.Setting
+    |> last(:inserted_at)
+    |> Lanpartyseating.Repo.one()
+
+    settings = Ecto.Changeset.change settings, rows: rows, columns: columns, row_padding: row_padding,
+      column_padding: column_padding, horizontal_trailing: horizontal_trailing, vertical_trailing: vertical_trailing
+    case Lanpartyseating.Repo.update settings do
+      {:ok, struct}       -> settings
+      {:error, changeset} -> nil
+    end
+  end
+
 end
