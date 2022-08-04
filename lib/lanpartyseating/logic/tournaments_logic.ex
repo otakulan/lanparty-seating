95244242d2f5b2a55ed2e609f979699ac90d1a21
Remi Martel <remi.martel.37@gmail.com>
2022-08-04 09:16:41 -0400

    added tournaments

--- lib/lanpartyseating/logic/tournaments_logic.ex
+++ lib/lanpartyseating/logic/tournaments_logic.ex
@@ -8,8 +8,16 @@ defmodule Lanpartyseating.TournamentsLogic do
   end
 
   def get_all_daily_tournaments do
+    now = DateTime.truncate(DateTime.utc_now(), :second)
+
+    date = elem(Date.new(now.year, now.month, now.day+1), 1)
+    time = elem(Time.new(4, 0, 0, 0), 1)
+    tomorrow = elem(DateTime.new(date, time, "Etc/UTC"), 1)
+
+    IO.inspect(tomorrow)
+
     Tournament
-    |> where([v], v.end_date > from_now(0, "second") )
+    |> where([v], v.end_date > from_now(0, "second") and v.end_date < ^tomorrow )
     |> where([v], is_nil(v.deleted_at))
     |> Repo.all()
   end
