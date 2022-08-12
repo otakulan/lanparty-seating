53c8e5eab0fc97faca08295efca9f1e8ca430832
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-08-11 22:39:41 -0400

    assets: generate tailwindcss in production

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule Lanpartyseating.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      "assets.deploy": ["cmd --cd assets npm run deploy", "esbuild default --minify", "phx.digest"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
