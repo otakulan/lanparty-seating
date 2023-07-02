823116bfb17e86e3e7408fe4320ce037fc9e026d
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2023-07-02 17:43:36 -0400

    almost completely implemented register_seat

--- lib/lanpartyseating/logic/seating_logic.ex
+++ lib/lanpartyseating/logic/seating_logic.ex
@@ -2,49 +2,27 @@ defmodule Lanpartyseating.SeatingLogic do
   import Ecto.Query
   alias Lanpartyseating.Station, as: Station
   alias Lanpartyseating.Seating, as: Seating
+  alias Lanpartyseating.BadgeScanLogs, as: BadgeScanLogs
+  alias Lanpartyseating.LastAssignedSeat, as: LastAssignedSeat
   alias Lanpartyseating.Repo, as: Repo
   alias Lanpartyseating.StationLogic, as: StationLogic
 
   def register_seat(badge_number) do
 
+    next_seat = ""
+
     if badge_number == "" do
       %{type: "error", message: "Please fill all the fields" }
     else
-
       # Get last assigned seat
-      las = Last_assigned_seat
+      las = LastAssignedSeat
       |> Repo.one()
-
-      #my_value = last_assigned_seat.last_assigned_seat
-
 
       # Get the next available seat
       next_seat = las.last_assigned_seat + 1
 
-      # TODO: update next availabe seat in DB
-      las = Ecto.Changeset.change las, last_assigned_seat: next_seat
-      case Repo.update las do
-        {:ok, result} -> result
-        {:error, _} -> nil
-      end
-
-
-      #station = Station
-      #|> where(station_number: ^seat_number)
-      #|> where([v], is_nil(v.deleted_at))
-      #|> Repo.one()
-
-      #isCreatable = case StationLogic.get_station_status(station.id).status do
-      #  "occupied"       -> false
-      #  "closed"         -> false
-      #  "available"      -> true
-      #end
-
-
-      # TODO: Reserve seat
-      # Participant gets his seat reserved
-      # Also log participant in badge_scans_logs table
-
+      # Log badge scans, seat ID is set to participant
+      expiry_time = DateTime.truncate(DateTime.utc_now() |> DateTime.add(45, :minute), :second)
 
       # --> badge number = his bage number
       # --> date scanned = time at which the request was created
@@ -53,11 +31,23 @@ defmodule Lanpartyseating.SeatingLogic do
       # --> was removed from AD: false until expiration
       # --> was cancelled: false unless sucessfully cancelled by a lan admin
       # --> date cancelled: logs the date when the cancel request was completed
+      Repo.insert(%BadgeScanLogs{badge_number: badge_number,
+                                 date_scanned: DateTime.truncate(DateTime.utc_now(), :second),
+                                 session_expiry: expiry_time,
+                                 assigned_station_number: next_seat,
+                                 was_removed_from_ad: false,
+                                 was_cancelled: false,
+                                 date_cancelled: nil
+                                 })
 
-
+      # The seat is registered to participant. Update the last reserved seat in DB.
+      las = Ecto.Changeset.change las, last_assigned_seat: next_seat, last_assigned_seat_date: DateTime.truncate(DateTime.utc_now(), :second)
+      case Repo.update las do
+        {:ok, result} -> result
+        {:error, _} -> nil
+      end
 
       # TODO: return seat ID, to be displayed on screen for the participant
-
 
       #if isCreatable == true do
       #  {:ok, updated} = Repo.insert(%Reservation{duration: duration, badge: badge_number, station_id: station.id})
@@ -67,7 +57,7 @@ defmodule Lanpartyseating.SeatingLogic do
       #end
     end
 
-    to_string(:rand.uniform(70))
+    to_string(next_seat)
   end
 
   def cancel_seat(seat_id, reason) do
