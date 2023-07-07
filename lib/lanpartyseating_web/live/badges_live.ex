5c3d83fa809ddf008c682c5a4a1f96d3ee4655d0
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2023-07-07 01:25:17 -0400

    Implement basic participants page

--- lib/lanpartyseating_web/live/badges_live.ex
+++ lib/lanpartyseating_web/live/badges_live.ex
@@ -23,6 +23,8 @@ defmodule LanpartyseatingWeb.BadgesLive do
         # fails to assign the requested seat.
         ReservationLogic.create_reservation(String.to_integer(assigned_station_id), 45, badge_number)
 
+        ## TODO: Create username and password in AD
+
         ## TODO: Display the ID of the reserved seat, all station have a username and password that relates to their ID
         ## The ID is the one of the next available station. People who come in group should scan their
         ## badge one after another if they want to be togheter.
--- lib/lanpartyseating_web/live/participants_live.ex
+++ lib/lanpartyseating_web/live/participants_live.ex
@@ -1,7 +1,15 @@
 defmodule LanpartyseatingWeb.ParticipantsLive do
   use LanpartyseatingWeb, :live_view
+  alias Lanpartyseating.BadgeScanLogsLogic, as: BadgeScanLogsLogic
 
   def mount(_params, _session, socket) do
+    participants = BadgeScanLogsLogic.get_all_participants()
+
+    socket =
+      socket
+      |> assign(:participants, Enum.reverse(participants))
+      |> assign(:participantsCount, length(participants))
+
     {:ok, socket}
   end
 
@@ -11,39 +19,43 @@ defmodule LanpartyseatingWeb.ParticipantsLive do
       <h1 style="font-size:30px">Seats</h1>
 
       <table>
-        <tr>
+      <tr>
           <th>badge id</th>
           <!-- badge uid -->
           <th>last scan</th>
           <!-- scan date that triggered the creation of the user in the AD -->
+
           <th>minutes left</th>
           <!-- minutes left until the user can't login or is logged off -->
+
+          <th>expiry</th>
+
+          <th>seat number</th>
+
           <th>deactivated</th>
           <!-- a boolean that's true once the user has been successfully deleted from the AD -->
           <th>username</th>
           <!-- username as the temporary login credential -->
         </tr>
+      <%= for participant <- @participants do %>
         <tr>
-          <td>00193</td>
-          <td>May 6 2023, 17:43</td>
-          <td>32</td>
-          <td>no</td>
-          <td>fish 🐟</td>
+          <th><%= participant.badge_number %></th>
+          <!-- badge uid -->
+          <th><%= participant.date_scanned %></th>
+          <!-- scan date that triggered the creation of the user in the AD -->
+          <th>?</th>
+          <!-- minutes left until the user can't login or is logged off -->
+
+          <th><%= participant.session_expiry %></th>
+
+          <th><%= participant.assigned_station_number %></th>
+
+          <th>?</th>
+          <!-- a boolean that's true once the user has been successfully deleted from the AD -->
+          <th>?</th>
+          <!-- username as the temporary login credential -->
         </tr>
-        <tr>
-          <td>04918</td>
-          <td>May 6 2023, 14:43</td>
-          <td>13</td>
-          <td>no</td>
-          <td>bee 🐝</td>
-        </tr>
-        <tr>
-          <td>03112</td>
-          <td>May 6 2023, 13:43</td>
-          <td>0</td>
-          <td>yes</td>
-          <td>sunflower 🌻</td>
-        </tr>
+        <% end %>
       </table>
     </div>
     """
