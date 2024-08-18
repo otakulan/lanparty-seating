038d5978f642a4b9226f043d3360fc5359e75b35
Babbaj <babbaj45@gmail.com>
2024-08-18 00:24:02 -0400

    simplify stations_status

--- lib/lanpartyseating/repositories/station_status_repo.ex
+++ lib/lanpartyseating/repositories/station_status_repo.ex
@@ -2,11 +2,10 @@ defmodule Lanpartyseating.StationStatus do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key {:id, :id, autogenerate: true}
-  @foreign_key_type :id
+  @primary_key {:station_id, :integer, autogenerate: false}
+  @foreign_key_type :integer
 
   schema "stations_status" do
-    field :station_id, :integer
     field :is_assigned, :boolean, default: false
     field :is_out_of_order, :boolean, default: false
     timestamps()
--- priv/repo/migrations/20240814023743_squash_migrations.exs
+++ priv/repo/migrations/20240814023743_squash_migrations.exs
@@ -62,12 +62,12 @@ defmodule Lanpartyseating.Repo.Migrations.SquashMigrations do
       timestamps()
     end
 
-    create table(:stations_status) do
+    create table(:stations_status, primary_key: false) do
       # Populate with all the IDs of the gaming stations. Set their assignation to true or false.
       # We can quickly retrive them to see which stations are occupied (e.g. to show all of them on a page)
       # or to quickly find the next available seat to assign a new participant.
       # A station can be set out of order if it stops working during the event.
-      add(:station_id, :integer)
+      add(:station_id, :integer, primary_key: true)
       add(:is_assigned, :boolean)
       add(:is_out_of_order, :boolean)
       timestamps()
