6d03c3f74439b59fe94173653fdb81ad9ae4982c
Nicolas Berbiche <nicolas@normie.dev>
2026-08-01 14:38:34 -0400

    Fix asset loading
    The favicon and other JS files weren't loading correctly anymore.
    
    Fix the static_paths/0 to properly expose the right folders to find
    the assets in.

--- lib/lanpartyseating_web.ex
+++ lib/lanpartyseating_web.ex
@@ -17,7 +17,7 @@ defmodule LanpartyseatingWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(css fonts images js favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
   def router do
     quote do
